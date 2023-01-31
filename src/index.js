49f8090feee1ed744b53c0108faa2d77d42df20e
Mr. Lawrence <lbajoie34@gmail.com>
2023-01-31 14:52:32 -0600

    All is well, learning to use props in my code writ

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 
-// write your Color component here
+
+const Color = (props) => {
+  return(
+    <div className={`selected ${props.color}`} onClick={() => props.selectColor(props.color)} />
+  )
+} // write your Color component here
 
 const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+  const selectColor = (colorName) => {
+  setSelectedColor(colorName);
+}
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className= {`selected ${selectedColor}`}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color='red'  selectColor={selectColor} />
+        <Color color='blue' selectColor={selectColor} />
+        <Color color='green' selectColor={selectColor} />
+        <Color color='orange' selectColor={selectColor} />
+      </div>
     </div>
   );
 }
